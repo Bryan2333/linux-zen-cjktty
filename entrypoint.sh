83156d45ef4a5ca473819c4d3ff206d2ed84f087
BryanLiang <liangrui.ch@gmail.com>
2024-09-11 10:44:09 +0800

    6.10.9.zen1-2: rebuild

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,7 +26,7 @@ chown builduser:builduser -R linux-zen
 
 cd linux-zen || exit 1
 
-builder_do "git reset --hard d0429d0"
+builder_do "git reset --hard 0c81f64"
 
 for KEYFILE in keys/pgp/*.asc
 do 
